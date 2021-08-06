ce913a75541f4e6767098d633c63fcc842271601
Kori <kori@koriroys.com>
2021-08-06 12:12:11 -0600

    fix bug where org would show up multiples times in response if it had multiple matching addresses

--- app/controllers/api/recipients_controller.rb
+++ app/controllers/api/recipients_controller.rb
@@ -4,7 +4,7 @@ module Api
       recipient_ids = Award.pluck(:recipient_id).uniq
 
       result = if params[:state]
-        orgs = Organization.joins(:addresses).where(id: recipient_ids).where("addresses.state = ?", params[:state])
+        orgs = Organization.joins(:addresses).where(id: recipient_ids).where("addresses.state = ?", params[:state]).uniq
         RecipientSerializer.new(orgs, {include: [:addresses]}).serializable_hash
       else
         RecipientSerializer.new(Organization.where(id: recipient_ids), {include: [:addresses]}).serializable_hash
